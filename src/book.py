1f72da47c1da9d1f9bb896c1e8b0349e4c78db57
mxmllnknz <mkunz778@gmail.com>
2023-06-15 16:38:37 -0700

    Initial commit

---
+++ src/book.py
@@ -0,0 +1,6 @@
+class Book:
+    def __init__(self, title, num_pages, author, year):
+        self.title = title
+        self.num_pages = num_pages
+        self.author = author
+        self.year = year
